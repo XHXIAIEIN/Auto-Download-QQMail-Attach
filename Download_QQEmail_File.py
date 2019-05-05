b2a0b5fb0ba00c033978b3f552ff614bffa4f95e
XHXIAIEIN <45864744+XHXIAIEIN@users.noreply.github.com>
2019-05-05 15:12:10 +0800

    Update Download_QQEmail_File.py
    updata

--- Download_QQEmail_File.py
+++ Download_QQEmail_File.py
@@ -4,6 +4,7 @@ import urllib
 import _thread
 import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 #......................................................
 # 自定义参数
@@ -12,46 +13,49 @@ from selenium import webdriver
 # https://mail.qq.com/cgi-bin/frame_html?t=frame_html&sid={ A }&url=/cgi-bin/mail_list?folderid={ B }%26page={ C }
 #
 # 需要自定义的参数已在上方链接以 A B C 标记出
-# A - token['sid']         相当于临时身份认证。这串密钥似乎会每天更新，每次使用时需要重新填写。
+# A - token['sid']         相当于临时身份认证。这串密钥会定时更新，每次使用时需要重新填写。
 # B - token['folderid']    文件夹的ID
-# C - token['page']        邮件列表页数
+# C - token['page']        邮件列表页数(初始页为0)
 #......................................................
 token={'sid':'R6-CnEyUYkXOuAy', 'folderid':135, 'page':0 }
-
-#......................................................
-# title['index']    第index封邮件
-# title['start']    从第start封邮件开始
-# title['end']      到第end封邮件结束，-1为默认列表长度
-# title['step']     读取邮件次数超过step时结束，-1为默认列表长度
-#......................................................
-title={'index':0, 'start':1, 'end':-1, 'step':-1}
-
 
 # 附件下载到哪个文件夹（需提前新建文件夹，不然会报错）
 download_here='D:\\Downloads\\email'
 
 #......................................................
-# 用来测试的一些参数
+# 用来测试的其他参数
 #......................................................
 
 # 开启DEBUG模式后，只输出列表数据，不下载任何附件。（0: 关闭 | 1: 开启）
 DEBUG=0
+
+#......................................................
+# title['index']    第index封邮件
+# title['start']    从第start封邮件开始
+# title['end']      到第end封邮件结束，默认为-1。
+# title['step']     读取邮件次数超过step时结束，默认为-1
+#......................................................
+title={'index':0, 'start':1, 'end':-1, 'step':-1}
+
 
 # 用来计数测试用的，总感觉有时会漏掉一些文件。
 test={'fileindex': 0, 'filecount': 0, 'downloadtimes': 0}
 
 # readmail  邮件列表：包含邮件id(value)、时间戳(totime)、发件人邮箱(fa)、发件人昵称(fn)
 # filemail  附件列表：包含邮箱、邮件主题、发件人昵称、附件名
-readmail,filemail=[],[]
+# foolmail  没有附件：包含发件人昵称
+readmail,filemail,foolmail=[],[],[]
+
 
 #......................................................
 # 配置Web Driver
 #......................................................
-prefs={"download.default_directory":download_here}
 options=webdriver.ChromeOptions()
+prefs={"profile.managed_default_content_settings.images":2,"download.default_directory":download_here}
 options.add_argument("user-data-dir=selenium")
-options.add_argument("Referrer-Policy=no-referrer")
 options.add_experimental_option("prefs",prefs)
+options.add_argument("--window-size=800,600")
+options.add_argument('--ignore-certificate-errors')
 chrome=webdriver.Chrome(options=options)
 
 url_qqmail='https://mail.qq.com/'
@@ -78,7 +82,7 @@ elements=chrome.find_elements_by_css_selector('input[name="mailid"]')
 # 获取邮件列表
 for e in elements:
     if title['index'] >= title['start']:
-        # check = (false, true)[a == -1]
+        # check = (false, true)[num == -1] // if (num == -1) TRUE else FALSE
         check_end  = (title['index'] <= title['end'],title['index'] <= len(elements))[title['end'] == -1]
         check_step = (title['index']-title['start'] < title['step'],True)[title['step'] == -1]
 
@@ -91,7 +95,7 @@ for e in elements:
           sender.update({'index': title['index']})
           readmail.append(sender)
           print(' ├─{} {}'.format(title['index'],sender['name']))
-          time.sleep(0.1)
+          time.sleep(1)
 
     title['index']+=1
 
@@ -104,18 +108,26 @@ print(" 开始处理邮件")
 # 获取每封邮件的附件列表
 for key in readmail:
     url=url_qqmail+'cgi-bin/frame_html?sid={}&url=/cgi-bin/readmail?mailid={}'.format(token['sid'],key['id'])
-    url=urllib.parse.unquote(url)
-    chrome.implicitly_wait(3)
     chrome.get(url)
     chrome.switch_to.default_content()
     chrome.switch_to.frame(chrome.find_element_by_id("mainFrame"))
 
+    title=chrome.find_element_by_id("subject").text
     elements=chrome.find_elements_by_class_name("name_big")
-    print(' ├─{} {}'.format(key['index'],key['name']))
+    isFool=len(elements)<=0
 
+    print(' ├─{} {} {}'.format(key['index'],key['name'],('', '(没有附件)')[isFool]))
+    
+    if isFool:
+        foolmail.append(key['name'])
+        mark_star=chrome.find_element_by_id("img_star")
+        if mark_star.get_attribute("class") == 'qm_ico_flagoff':
+            mark_star.send_keys(Keys.SPACE)
+        continue
+    
     for f in elements:
         attach={}
-        attach.update({'title': chrome.find_element_by_id("subject").text})
+        attach.update({'title': title})
         attach.update({'name': key['name']})
         attach.update({'email': key['email']})
         attach.update({'filename': f.find_element_by_css_selector('span:nth-child(1)').text})
@@ -123,13 +135,13 @@ for key in readmail:
         test['filecount']+=1
         print(" │  ├─{}".format(attach['filename']))
 
-
     if DEBUG != 1:
         os.chdir(download_here)
         cmd=open("_ren.bat","a")
         cmd.seek(0)
         cmd.truncate()
         cmd.write("@echo off")
+        cmd.write("\n")
 
         for key in filemail:
             cmd.write('ren "{}" "{}-{}"'.format(key['filename'], key['email'], key['filename']))
@@ -145,5 +157,4 @@ for key in readmail:
             test['downloadtimes']+=1
             time.sleep(2)
 
-
-print(' └─filecount:{}    downloadtimes:{}'.format(test['filecount'],test['downloadtimes']))
+print(' └─Emailecount:{}    Foolcount:{}    filecount:{}    downloadtimes:{}'.format(len(readmail), len(foolmail), test['filecount'],test['downloadtimes']))
